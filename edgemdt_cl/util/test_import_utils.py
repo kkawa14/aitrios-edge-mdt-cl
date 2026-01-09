0f049c9dfdefdb2991ade7f89266d64f301d4375
kawasaki.kenta <kawasaki.kenta@miraxia.com>
2026-01-09 12:10:07 +0900

    modify test code

--- edgemdt_cl/util/test_import_utils.py
+++ edgemdt_cl/util/test_import_utils.py
@@ -24,8 +24,23 @@ from edgemdt_cl.util.import_util import (
 
 
 class TestImportLibsCheck:
+    @staticmethod
+    def _create_mock_import_module(mock_modules):
+        def mock_import_module(name):
+            if name not in mock_modules:
+                raise ImportError(f"No module named '{name}'")
 
-    TEST_CASES = [
+            module = MagicMock()
+            version = mock_modules[name]
+
+            if version is not None:
+                module.__version__ = version
+            else:
+                del module.__version__
+            return module
+        return mock_import_module
+
+    @pytest.mark.parametrize(("requirements", "mock_modules", "boolean_expected", "error_message_expected"), [
         ### torch case
         (['torch'], {'torch': '2.3.0'}, True, None),
         (['torch>=2.3'], {'torch': '2.3.0'}, True, None),
@@ -68,43 +83,18 @@ class TestImportLibsCheck:
         (['onnx>=1.14,<1.18'], {'onnx': '1.17.0a0+dev0'}, True, None),
         (['onnx>=1.14,<1.18'], {'onnx': '1.3.0'}, False, "\nRequired 'onnx' version <1.18,>=1.14, installed version 1.3.0."),
         (['onnx>=1.14,<1.18'], {'onnx': '1.3.0+dev1'}, False, "\nRequired 'onnx' version <1.18,>=1.14, installed version 1.3.0+dev1."),
-    ]
-
-    @staticmethod
-    def _create_mock_import_module(mock_modules):
-        def mock_import_module(name):
-            if name not in mock_modules:
-                raise ImportError(f"No module named '{name}'")
-        
-            module = MagicMock()
-            version = mock_modules[name]
-
-            if version is not None:
-                module.__version__ = version
-            else:
-                del module.__version__
-            return module
-        return mock_import_module
-
-    @pytest.mark.parametrize(("requirements", "mock_modules", "expected"), 
-        [(req, mock, expected) for req, mock, expected, _ in TEST_CASES]
-    )
-    def test_is_compatible(self, requirements, mock_modules, expected):
+    ])
+    def test_validate_installed_libraries(self, requirements, mock_modules, boolean_expected, error_message_expected):
         mock_import_module = self._create_mock_import_module(mock_modules)
         
         with patch('edgemdt_cl.util.import_util.importlib.import_module', side_effect=mock_import_module):
-            assert is_compatible(requirements) == expected
+            ### comparison using is_compatible()
+            assert is_compatible(requirements) == boolean_expected
 
-    @pytest.mark.parametrize(("requirements", "mock_modules", "expected"),
-        [(req, mock, expected) for req, mock, _, expected in TEST_CASES]
-    )
-    def test_validate_installed_libraries(self, requirements, mock_modules, expected):
-        mock_import_module = self._create_mock_import_module(mock_modules)
-        
-        with patch('edgemdt_cl.util.import_util.importlib.import_module', side_effect=mock_import_module):
-            if expected is not None:
+            ### comparison using validate_installed_libraries()
+            if error_message_expected is not None:
                 with pytest.raises(RequirementError) as e:
                     validate_installed_libraries(requirements)
-                assert str(e.value) == expected
+                assert str(e.value) == error_message_expected
             else:
                 validate_installed_libraries(requirements)
